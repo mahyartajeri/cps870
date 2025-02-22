8066d44aa5e7939e7c5f874e0ad8d3541131f876
unknown <mahyar.tajeri@gmail.com>
2025-02-22 16:57:13 -0500

    initial commit

---
+++ A2/example_pyppeteer.py
@@ -0,0 +1,26 @@
+# if pyppeteer gives some chromium error, follow instructions here https://github.com/pyppeteer/pyppeteer/issues/463#issuecomment-1962365815 (match version in error)
+
+
+import asyncio
+from pyppeteer import launch
+
+
+async def main():
+    browser = await launch()
+    page = await browser.newPage()
+    await page.goto('https://example.com')
+    await page.screenshot({'path': 'example.png'})
+
+    dimensions = await page.evaluate('''() => {
+        return {
+            width: document.documentElement.clientWidth,
+            height: document.documentElement.clientHeight,
+            deviceScaleFactor: window.devicePixelRatio,
+        }
+    }''')
+
+    print(dimensions)
+    # >>> {'width': 800, 'height': 600, 'deviceScaleFactor': 1}
+    await browser.close()
+
+asyncio.get_event_loop().run_until_complete(main())
